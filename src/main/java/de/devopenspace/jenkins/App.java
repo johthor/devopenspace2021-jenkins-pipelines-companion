7a46e863ca72fe44da9a6e8f1deee63585aa7756
Johannes Thorn <johthor@users.noreply.github.com>
2021-11-27 21:55:39 +0100

    Add more violations

--- src/main/java/de/devopenspace/jenkins/App.java
+++ src/main/java/de/devopenspace/jenkins/App.java
@@ -1,5 +1,7 @@
 package de.devopenspace.jenkins;
 
+import java.util.Collections;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        var name = "Workshop";
         System.out.println( "Hello World!" );
     }
 }
